51ef3cc274904d1315bfc3a3b666f50f1a881b74
Mikhail Istomin <mikhail_istomin@epam.com>
2023-05-11 00:49:31 +0400

    replace node API with lambda

--- src/reminder/reminder.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ReminderController } from './reminder.controller';
-
-@Module({
-  controllers: [ReminderController]
-})
-export class ReminderModule {}
