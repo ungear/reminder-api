42f288a74a884c9b362e7df208698c2ab656338e
Mikhail Istomin <mikhail_istomin@epam.com>
2023-05-16 14:28:46 +0400

    add data samples in responses

--- remindersApi/handler.js
+++ remindersApi/handler.js
@@ -1,21 +1,28 @@
 'use strict';
 
 module.exports.getAll = async (event) => {
+  const userEmail = event.requestContext.authorizer.jwt.claims.email;
+  const reminders = [
+    { id: 1, triggerTime: new Date().getTime(), text: 'first reminder text' },
+    { id: 2, triggerTime: new Date().getTime(), text: 'second reminder text' },
+  ]
   return {
     statusCode: 200,
     headers: {
       "content-type":"application/json",
     },
-    body: JSON.stringify({text: 'get reminders'})
+    body: JSON.stringify(reminders)
   };
 }
 
 module.exports.create = async (event) => {
+  const userEmail = event.requestContext.authorizer.jwt.claims.email;
+  const reminderBodyParams = JSON.parse(event.body);
   return {
     statusCode: 200,
     headers: {
       "content-type":"application/json",
     },
-    body: JSON.stringify({text: 'create reminder'})
+    body: JSON.stringify(reminderBodyParams)
   };
 }
