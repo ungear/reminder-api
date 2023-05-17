63b3ed051b30aefaa9dd2294fb46160c93080d02
Mikhail Istomin <mikhail_istomin@epam.com>
2023-05-17 19:15:10 +0400

    add "create" endpoint

--- remindersApi/handler.js
+++ remindersApi/handler.js
@@ -27,11 +27,27 @@ module.exports.getAll = async (event) => {
 module.exports.create = async (event) => {
   const userEmail = event.requestContext.authorizer.jwt.claims.email;
   const reminderBodyParams = JSON.parse(event.body);
+
+  // TODO: validate input
+
+  const reminderUuid = AWS.util.uuid.v4();
+  const newReminderParams = {
+    TableName: process.env.REMINDERS_DB_NAME,
+    Item: {
+      id: reminderUuid,
+      owner: userEmail,
+      triggerTime: reminderBodyParams.triggerTime,
+      text: reminderBodyParams.text,
+    },
+  };
+  await docClient.put(newReminderParams).promise();
+
+  // TODO: return result
   return {
     statusCode: 200,
-    headers: {
-      "content-type":"application/json",
-    },
-    body: JSON.stringify(reminderBodyParams)
+    // headers: {
+    //   "content-type":"application/json",
+    // },
+    // body: JSON.stringify(reminderBodyParams)
   };
 }
