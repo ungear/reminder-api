23f2ba292da994bbfc32d249ad73f94cad13de9a
Mikhail Istomin <mikhail_istomin@epam.com>
2023-05-17 19:06:21 +0400

    create db via serverless

--- remindersApi/handler.js
+++ remindersApi/handler.js
@@ -1,17 +1,26 @@
 'use strict';
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient();
 
 module.exports.getAll = async (event) => {
-  const userEmail = event.requestContext.authorizer.jwt.claims.email;
-  const reminders = [
-    { id: 1, triggerTime: new Date().getTime(), text: 'first reminder text' },
-    { id: 2, triggerTime: new Date().getTime(), text: 'second reminder text' },
-  ]
+  // email is needed for future use
+  // const userEmail = event.requestContext.authorizer.jwt.claims.email;
+  const productsData = await docClient.scan({
+    TableName : process.env.REMINDERS_DB_NAME
+  })
+    .promise();
+
+  // const reminders = [
+  //   { id: 1, triggerTime: new Date().getTime(), text: 'first reminder text' },
+  //   { id: 2, triggerTime: new Date().getTime(), text: 'second reminder text' },
+  // ]
+
   return {
     statusCode: 200,
     headers: {
       "content-type":"application/json",
     },
-    body: JSON.stringify(reminders)
+    body: JSON.stringify(productsData.Items)
   };
 }
 
